e9c1d331e1bc42e62221fe72d83afaa79efb5d98
Yanming Wang <yanmwang@amazon.com>
2022-03-16 17:40:56 +0000

    More change

--- xla_benchmark/profile.sh
+++ xla_benchmark/profile.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-nvprof ../bazel-bin/tensorflow/compiler/xla/tools/replay_computation_gpu --use_fake_data=true --num_runs=100 --print_result=false $1
+export XLA_FLAGS="--xla_dump_to=./hlo_graph --xla_dump_hlo_as_text --xla_dump_hlo_pass_re=.*"
+nvprof ../bazel-bin/tensorflow/compiler/xla/tools/replay_computation_gpu --use_fake_data=true --num_runs=1 --print_result=false $1
